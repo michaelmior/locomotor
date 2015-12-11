431fbd05e07f4775dd785ab90672618f6c1fe04a
Michael Mior <mmior@uwaterloo.ca>
2015-12-11 12:15:51 -0500

    Update doDelivery in TPC-C benchmark

--- bench/tpcc.py
+++ bench/tpcc.py
@@ -32,24 +32,30 @@ class PartitionedDriver(redisdriver.RedisDriver):
         self._doDelivery(rdr, wtr, params)
 
     @redis_server
-    def _doDelivery(self, rdr, wtr, params):
+    def _doDelivery(self, rdr, wtr, params) :
+        if self.debug['delivery'] != 'None' :
+            print 'TXN DELIVERY STARTING ------------------'
+            tt = time.time()
+        if self.debug['delivery'] == 'Verbose' :
+            t0 = tt
+
         # Initialize input parameters
         w_id = params["w_id"]
         o_carrier_id = params["o_carrier_id"]
         ol_delivery_d = params["ol_delivery_d"]
 
         # Initialize result set
-        result = []
+        result = [ ]
 
         #-------------------------
         # Initialize Data Holders
         #-------------------------
-        order_key = []
-        ol_total = []
-        customer_key = []
-        ol_counts = []
-        no_o_id = []
-        for d_id in range(1, constants.DISTRICTS_PER_WAREHOUSE + 1):
+        order_key = [ ]
+        ol_total = [ ]
+        customer_key = [ ]
+        ol_counts = [ ]
+        no_o_id = [ ]
+        for d_id in range(1, constants.DISTRICTS_PER_WAREHOUSE + 1) :
             order_key.append(None)
             ol_total.append(0)
             customer_key.append(None)
@@ -58,60 +64,59 @@ class PartitionedDriver(redisdriver.RedisDriver):
         #---------------------
         # Get New Order Query
         #---------------------
-        for d_id in range(1, constants.DISTRICTS_PER_WAREHOUSE + 1):
+        for d_id in range(1, constants.DISTRICTS_PER_WAREHOUSE + 1) :
             cursor = d_id - 1
             # Get set of possible new order ids
             index_key = self.safeKey([d_id, w_id])
             rdr.srandmember('NEW_ORDER.INDEXES.GETNEWORDER.' + index_key)
         id_set = rdr.execute()
 
-        for d_id in range(1, constants.DISTRICTS_PER_WAREHOUSE + 1):
+        for d_id in range(1, constants.DISTRICTS_PER_WAREHOUSE + 1) :	
             cursor = d_id - 1
             if not id_set[cursor]:
                 rdr.get('NULL_VALUE')
-            else:
+            else :
                 rdr.hget('NEW_ORDER.' + str(id_set[cursor]), 'NO_O_ID')
         no_o_id = rdr.execute()
 
-        if self.debug['delivery'] == 'Verbose':
-            pass
+        if self.debug['delivery'] == 'Verbose' :
+            print 'New Order Query: ', time.time() - t0
+            t0 = time.time()
 
         #-----------------------
         # Get Customer ID Query
         #-----------------------
-        for d_id in range(1, constants.DISTRICTS_PER_WAREHOUSE + 1):
+        for d_id in range(1, constants.DISTRICTS_PER_WAREHOUSE + 1) :	
             cursor = d_id - 1
             if not no_o_id[cursor]:
-                order_key.insert(cursor, 'NO_KEY')
-            else:
-                order_key.insert(
-                        cursor,
-                        self.safeKey([w_id, d_id, no_o_id[0]])
-                        )
-                rdr.hget('ORDERS.' + order_key[cursor], 'O_C_ID')
+                order_key[cursor] = 'NO_KEY'
+            else :
+                order_key[cursor] = self.safeKey([w_id, d_id, no_o_id[0]])
+            rdr.hget('ORDERS.' + order_key[cursor], 'O_C_ID')
         c_id = rdr.execute()
 
-        for d_id in range(1, constants.DISTRICTS_PER_WAREHOUSE + 1):
+        for d_id in range(1, constants.DISTRICTS_PER_WAREHOUSE + 1) :	
             cursor = d_id - 1
             if not no_o_id[cursor] or not c_id[cursor]:
                 si_key = 'NO_KEY'
-            else:
-                si_key = self.safeKey([no_o_id[cursor], w_id, d_id])
+            else :
+                si_key = self.safeKey([no_o_id[cursor], d_id, w_id])
             rdr.smembers('ORDER_LINE.INDEXES.SUMOLAMOUNT.' + si_key)
         ol_ids = rdr.execute()
 
-        if self.debug['delivery'] == 'Verbose':
-            pass
+        if self.debug['delivery'] == 'Verbose' :
+            print 'Get Customer ID Query:', time.time() - t0
+            t0 = time.time()
 
         #-----------------------------
         # Sum Order Line Amount Query
         #-----------------------------
-        for d_id in range(1, constants.DISTRICTS_PER_WAREHOUSE + 1):
+        for d_id in range(1, constants.DISTRICTS_PER_WAREHOUSE + 1) :	
             cursor = d_id - 1
             if not no_o_id[cursor] or not c_id[cursor]:
                 rdr.get('NULL_VALUE')
-            else:
-                for i in ol_ids[cursor]:
+            else :
+                for i in ol_ids[cursor] :
                     rdr.hget('ORDER_LINE.' + str(i), 'OL_AMOUNT')
                     ol_counts[cursor] += 1
 
@@ -119,21 +124,22 @@ class PartitionedDriver(redisdriver.RedisDriver):
         index = 0
         counter = 0
 
-        for ol_amount in pipe_results :
+        for ol_amount in pipe_results : 
             counter += 1
-            if counter > ol_counts[index]:
+            if counter > ol_counts[index] :
                 index += 1
                 counter = 0
             elif ol_amount:
                 ol_total[index] += float(ol_amount)
 
-        if self.debug['delivery'] == 'Verbose':
-            pass
+        if self.debug['delivery'] == 'Verbose' :
+            print 'Sum Order Line Query:', time.time() - t0
+            t0 = time.time()
 
-        for d_id in range(1, constants.DISTRICTS_PER_WAREHOUSE + 1):
+        for d_id in range(1, constants.DISTRICTS_PER_WAREHOUSE + 1) :	
             cursor = d_id - 1
             if not no_o_id[cursor] or not c_id[cursor]:
-                ## No orders for this district: skip it.
+                ## No orders for this district: skip it. 
                 ## Note: This must be reported if > 1%
                 continue
 
@@ -146,71 +152,72 @@ class PartitionedDriver(redisdriver.RedisDriver):
             wtr.srem('NEW_ORDER.IDS', no_key)
             wtr.srem('NEW_ORDER.INDEXES.GETNEWORDER.' + no_si_key, no_key)
 
-            if self.debug['delivery'] == 'Verbose':
-                pass
+            if self.debug['delivery'] == 'Verbose' :
+                print 'Delete New Order Query:', time.time() - t0
+                t0 = time.time()
 
             #---------------------
             # Update Orders Query
             #---------------------
             wtr.hset(
-                    'ORDERS.' + order_key[cursor],
-                    'W_CARRIER_ID',
+                    'ORDERS.' + order_key[cursor], 
+                    'W_CARRIER_ID', 
                     o_carrier_id
                     )
 
-            if self.debug['delivery'] == 'Verbose':
-                pass
+            if self.debug['delivery'] == 'Verbose' :
+                print 'Update Orders Query:', time.time() - t0
+                t0 = time.time()
 
             #-------------------------
             # Update Order Line Query
             #-------------------------
-            for i in ol_ids[cursor]:
+            for i in ol_ids[cursor] :
                 wtr.hset(
                         'ORDER_LINE.' + str(i),
-                        'OL_DELIVERY_D',
+                        'OL_DELIVERY_D', 
                         ol_delivery_d
                         )
 
-                if self.debug['delivery'] == 'Verbose':
-                    pass
+                if self.debug['delivery'] == 'Verbose' :
+                    print 'Update Order Line Query:', time.time() - t0
+                t0 = time.time()
+        wtr.execute()
 
         #-----------------------
         # Update Customer Query
         #-----------------------
-        for d_id in range(1, constants.DISTRICTS_PER_WAREHOUSE + 1):
+        for d_id in range(1, constants.DISTRICTS_PER_WAREHOUSE + 1) :	
             cursor = d_id - 1
             if not no_o_id[cursor] or not c_id[cursor]:
                 rdr.get('NULL_VALUE')
-                customer_key.insert(cursor, 'NO_KEY')
-            else:
-                customer_key.insert(
-                        cursor,
-                        self.safeKey([w_id, d_id, c_id[cursor]])
-                        )
+                customer_key[cursor] = 'NO_KEY'
+            else :
+                customer_key[cursor] = self.safeKey([w_id, d_id, c_id[cursor]])
                 rdr.hget('CUSTOMER.' + customer_key[cursor], 'C_BALANCE')
         old_balance = rdr.execute()
 
-        for d_id in range(1, constants.DISTRICTS_PER_WAREHOUSE + 1):
+        for d_id in range(1, constants.DISTRICTS_PER_WAREHOUSE + 1) :
             cursor = d_id - 1
             if not no_o_id[cursor] or not c_id[cursor]:
                 continue
-            else:
-                new_balance = float(old_balance[cursor]) + \
-                              float(ol_total[cursor])
+            else :
+                new_balance = float(old_balance[cursor]) + float(ol_total[cursor])
                 wtr.hset(
                         'CUSTOMER.' + customer_key[cursor],
-                        'C_BALANCE',
+                        'C_BALANCE', 
                         new_balance
                         )
                 result.append((d_id, no_o_id[cursor]))
         wtr.execute()
 
-        if self.debug['delivery'] == 'Verbose':
-            pass
-        if self.debug['delivery'] != 'None':
-            pass
+        if self.debug['delivery'] == 'Verbose' :
+            print 'Update Customer Query:', time.time() - t0
+        if self.debug['delivery'] != 'None' :
+            print 'TXN DELIVERY:', time.time() - tt
 
         return result
+    # End doDelivery()
 
 def bench(partition=False):
     global nurand
